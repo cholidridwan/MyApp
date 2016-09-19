1b8c3aa8bb4191f597b522c54eea3050b1f7af80
cholid ridwan <cholid@id.edge-asia.com>
2016-09-19 18:11:20 +0700

    add method to mkdir folder if not exist and check it if any file in it

--- app/src/main/java/com/edgeasia/myapplication/MainActivity.java
+++ app/src/main/java/com/edgeasia/myapplication/MainActivity.java
@@ -1,10 +1,18 @@
 package com.edgeasia.myapplication;
 
+import android.os.Environment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    String fullPath = Environment.getExternalStorageDirectory().getPath() + "/Android/data/com.edge.myapp/";
+    final File newFolder = new File(fullPath);
+    final File[] contents = newFolder.listFiles();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
         ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPageAndroid);
         ImageAdapter adapterView = new ImageAdapter(this);
         mViewPager.setAdapter(adapterView);
+
+        if (!newFolder.exists()) {
+            Log.i("mkdir", "success");
+            newFolder.mkdir();
+        }
+
+        if (contents.length == 0) {
+            Log.i("contain", "0");
+        }else {
+            Log.i("contain", String.valueOf(contents.length) + ":" + contents.toString());
+        }
+
     }
 }
