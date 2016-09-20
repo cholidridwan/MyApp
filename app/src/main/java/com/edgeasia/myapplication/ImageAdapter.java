fa16a648ae32157d67fdb93b463bd6a7473ca9db
cholid ridwan <cholid@id.edge-asia.com>
2016-09-20 11:50:02 +0700

    add complete app, show default image when has no file in folder

--- app/src/main/java/com/edgeasia/myapplication/ImageAdapter.java
+++ app/src/main/java/com/edgeasia/myapplication/ImageAdapter.java
@@ -1,31 +1,54 @@
 package com.edgeasia.myapplication;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.io.File;
+
 /**
  * Created by cholid_ridwan on 9/19/2016.
  */
 public class ImageAdapter extends PagerAdapter {
     Context mContext;
+    private int[] sliderImagesId = new int[]{
+            R.drawable.p01, R.drawable.p03, R.drawable.p05,
+            R.drawable.p02, R.drawable.p04, R.drawable.p06
+    };
+    private boolean setDefault = true;
+    private int contentLength;
+    private File imgFile;
+    private String[] nameFile;
+    private Bitmap myBitmap;
 
     ImageAdapter(Context context) {
         this.mContext = context;
     }
 
-    @Override
-    public int getCount() {
-        return sliderImagesId.length;
+    public void setImage(String[] name){
+        this.nameFile = name;
     }
 
-    private int[] sliderImagesId = new int[]{
-            R.drawable.p01, R.drawable.p03, R.drawable.p05,
-            R.drawable.p02, R.drawable.p04, R.drawable.p06
-    };
+    public void setSetDefault(boolean boo){
+        setDefault = boo;
+    }
+
+    public void setContentLength(int contentLength){
+        this.contentLength = contentLength;
+    }
+
+    @Override
+    public int getCount() {
+        if(!setDefault){
+            return contentLength;
+        }
+        return sliderImagesId.length;
+    }
 
     @Override
     public boolean isViewFromObject(View v, Object obj) {
@@ -36,7 +59,13 @@ public class ImageAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int i) {
         ImageView mImageView = new ImageView(mContext);
         mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        mImageView.setImageResource(sliderImagesId[i]);
+        if(!setDefault) {
+            imgFile = new File(nameFile[i]);
+            myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            mImageView.setImageBitmap(myBitmap);
+        }else{
+            mImageView.setImageResource(sliderImagesId[i]);
+        }
         ((ViewPager) container).addView(mImageView, 0);
         return mImageView;
     }
--- app/src/main/java/com/edgeasia/myapplication/MainActivity.java
+++ app/src/main/java/com/edgeasia/myapplication/MainActivity.java
@@ -14,14 +14,16 @@ public class MainActivity extends AppCompatActivity {
     final File newFolder = new File(fullPath);
     final File[] contents = newFolder.listFiles();
 
+    private String[] mFileStrings;
+    private File[] listFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewPageAndroid);
-        ImageAdapter adapterView = new ImageAdapter(this);
-        mViewPager.setAdapter(adapterView);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPageAndroid);
+        ImageAdapter imageAdapter = new ImageAdapter(this);
 
         if (!newFolder.exists()) {
             Log.i("mkdir", "success");
@@ -30,9 +32,27 @@ public class MainActivity extends AppCompatActivity {
 
         if (contents.length == 0) {
             Log.i("contain", "0");
+            imageAdapter.setSetDefault(true);
         }else {
             Log.i("contain", String.valueOf(contents.length) + ":" + contents.toString());
+            imageAdapter.setSetDefault(false);
+            imageAdapter.setContentLength(contents.length);
+            File file = new File(fullPath);
+
+            if (file.isDirectory())
+            {
+                listFile = file.listFiles();
+                mFileStrings = new String[listFile.length];
+
+                for (int i = 0; i < listFile.length; i++)
+                {
+                    mFileStrings[i] = listFile[i].getAbsolutePath();
+                    imageAdapter.setImage(mFileStrings);
+                }
+            }
         }
+
+        viewPager.setAdapter(imageAdapter);
 
     }
 }
