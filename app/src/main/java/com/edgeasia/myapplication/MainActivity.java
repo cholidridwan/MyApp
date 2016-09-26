555b9d4054dfdc8c926727322834ddbf3dc6faf8
cholid ridwan <cholid@id.edge-asia.com>
2016-09-26 12:02:20 +0700

    add delete app, open and delete

--- app/src/main/java/com/edgeasia/myapplication/MainActivity.java
+++ app/src/main/java/com/edgeasia/myapplication/MainActivity.java
@@ -7,13 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    String fullPath = Environment.getExternalStorageDirectory().getPath() + "/Android/data/com.edge.myapp/";
+    final String fullPath = Environment.getExternalStorageDirectory().getPath() + "/Android/data/com.edge.myapp/";
     final File newFolder = new File(fullPath);
     final File[] contents = newFolder.listFiles();
 
@@ -66,6 +67,79 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //viewPager.setCurrentItem(3, false);
                 Log.i("page", String.valueOf(viewPager.getCurrentItem()));
+            }
+        });
+
+        final EditText nameApp = (EditText)findViewById(R.id.nameApp);
+
+        Button btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setAlpha(0.5f); // it can set to ZERO
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String customPath = Environment.getExternalStorageDirectory().getPath() + "/Android/data/com.edge." + nameApp.getText();
+                File customFolder = new File(customPath);
+                File[] customContents = customFolder.listFiles();
+
+                if (!customFolder.exists()) {
+                    Log.i("mkdir", "success");
+                    customFolder.mkdir();
+                }
+            }
+        });
+
+        Button btnOpen = (Button) findViewById(R.id.btnOpen);
+        btnOpen.setAlpha(0.5f); // it can set to ZERO
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String customPath = Environment.getExternalStorageDirectory().getPath() + "/Android/data/com.edge." + nameApp.getText();
+                File customFolder = new File(customPath);
+                File[] customContents = customFolder.listFiles();
+
+                if (customContents.length == 0) {
+                    Log.i("contain", "0");
+                    imageAdapter.setSetDefault(true);
+                }else {
+                    Log.i("contain", String.valueOf(customContents.length) + ":" + customContents.toString());
+                    imageAdapter.setSetDefault(false);
+                    imageAdapter.setContentLength(customContents.length);
+                    File file = new File(fullPath);
+
+                    if (file.isDirectory())
+                    {
+                        listFile = file.listFiles();
+                        mFileStrings = new String[listFile.length];
+
+                        for (int i = 0; i < listFile.length; i++)
+                        {
+                            mFileStrings[i] = listFile[i].getAbsolutePath();
+                            imageAdapter.setImage(mFileStrings);
+                        }
+                    }
+                }
+
+                viewPager.setAdapter(imageAdapter);
+            }
+        });
+
+        Button btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnDelete.setAlpha(0.5f); // it can set to ZERO
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String customPath = Environment.getExternalStorageDirectory().getPath() + "/Android/data/com.edge." + nameApp.getText();
+                File customFolder = new File(customPath);
+                if (customFolder.isDirectory())
+                {
+                    String[] children = customFolder.list();
+                    for (int i = 0; i < children.length; i++)
+                    {
+                        new File(customFolder, children[i]).delete();
+                    }
+                }
+
+                customFolder.delete();
             }
         });
     }
